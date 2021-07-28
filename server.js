29aec87db3674d8118859a5b5b64e25676985676
Anastasia <nadymova.anastasiya@gmail.com>
2021-07-28 04:21:04 +0300

    Add render page

--- server.js
+++ server.js
@@ -10,9 +10,13 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'pug');
 
 app.route('/').get((req, res) => {
-  res.render('Load your view here');
+  res.render(process.cwd() + '/views/pug/index', {
+    title: 'Hello',
+    message: 'Please login'
+  });
 });
 
 const PORT = process.env.PORT || 3000;
